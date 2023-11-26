ab20c5e5d3bf6ff6ff2848fb07fff752adef6b07
Kiran Shila <me@kiranshila.com>
2023-11-26 13:33:59 -0800

    Add openscad to syntax highlight

--- resources/public/worker.js
+++ resources/public/worker.js
@@ -1,19 +1,23 @@
 onmessage = function (event) {
   importScripts(
-    "//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/highlight.min.js"
+    "//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/highlight.min.js",
   );
   importScripts(
-    "//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/languages/julia.min.js"
+    "//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/languages/julia.min.js",
   );
   importScripts(
-    "//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/languages/julia-repl.min.js"
+    "//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/languages/julia-repl.min.js",
   );
   importScripts(
-    "//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/languages/clojure.min.js"
+    "//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/languages/clojure.min.js",
   );
   importScripts(
-    "//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/languages/matlab.min.js"
+    "//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/languages/matlab.min.js",
   );
+  importScripts(
+    "//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/languages/openscad.min.js",
+  );
+
   self.hljs.configure({ tabReplace: 4 });
   var result = self.hljs.highlightAuto(event.data);
   postMessage(result.value);
